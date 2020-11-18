0db237c80a3060c7176d4bbeec7f0d79d2bc8160
Administrador <midoiaga@gmail.com>
2020-11-18 13:16:59 +0100

    3 .fxml desberdin bakoitza kudeatzaile desberdinarekin kudeatzeko.

---
+++ src/main/java/ehu/isad/Controllers/UI/WebKud.java
@@ -0,0 +1,75 @@
+package ehu.isad.Controllers.UI;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+public class WebKud implements Initializable {
+
+    @FXML
+    private GridPane pnWhatWeb;
+
+    @FXML
+    private TextField textURL;
+
+    @FXML
+    private TextArea textAreaLog;
+
+    @FXML
+    private Button btnScan;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        textAreaLog.setWrapText(true);
+
+    }
+    @FXML
+    void onClickScan(ActionEvent event) {
+        String newLine = System.getProperty("line.separator");
+        textAreaLog.setText( allProcesses().stream()
+                .collect(Collectors.joining(newLine)) );
+
+    }
+
+
+    public List<String> allProcesses() {
+        List<String> processes = new LinkedList<String>();
+        try {
+            String line;
+            Process p = null;
+            if(System.getProperty("os.name").toLowerCase().contains("win")) {
+                p = Runtime.getRuntime().exec
+                        (System.getenv("windir") +"\\system32\\"+"" +
+                                "wsl whatweb --color=never "+this.textURL.getText());
+            } else {
+                p = Runtime.getRuntime().exec("whatweb --color=never "+this.textURL.getText());
+            }
+            BufferedReader input =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                processes.add(line);
+            }
+            input.close();
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+
+        return processes;
+    }
+    public WebKud() {
+        System.out.println("web inst");
+    }
+}
